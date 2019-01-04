b31b1f4e5dbb2568f9cf8cd9ab5223223175c630
Ashley Trowell <ajtrowell@gmail.com>
2019-01-04 17:16:37 -0600

    Added basic paint loop.
    Ball moves offscreen, needs
    code to support a bounce.

--- src/Game.java
+++ src/Game.java
@@ -4,28 +4,43 @@ import java.awt.geom.Ellipse2D;
 
 public class Game extends JPanel {
 
+    int x = 0;
+    int y = 0;
+
+    private void moveBall() {
+        x++;
+        y++;
+    }
+
 
     @Override
     public void paint(Graphics g) {
+        super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(Color.RED);
-        g2d.fillOval(0,0,30,30);
-        g2d.drawOval(0,50,30,30);
-        g2d.fillRect(50,0,30,30);
-        g2d.drawRect(50,50,30,30);
-
-        g2d.draw(new Ellipse2D.Double(0,100,30,30));
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.fillOval(x,y,30,30);
     }
 
 
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         JFrame frame = new JFrame("Mini Tennis");
-        frame.add(new Game());
-        frame.setSize(300,300);
+        Game game = new Game();
+        frame.add(game);
+        frame.setSize(300,400);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        while (true) {
+            game.moveBall();
+            game.repaint();
+            try {
+                Thread.sleep(10);
+            } catch (Exception e) {
+                    System.out.print(e);
+            }
+        }
 
     }
 }
